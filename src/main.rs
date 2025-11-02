2ef47fa9a6e4a342ef3a7dd21e164b28f5ab9655
Hackcraft_ <hackcraft_@hotmail.com>
2025-11-02 14:24:21 -0500

    feat: some rudimentary gravity
    Signed-off-by: Hackcraft_ <hackcraft_@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::f32::consts::PI;
 
-use bevy::{
-    input::common_conditions::input_just_pressed, log::tracing_subscriber::fmt::time, prelude::*,
-    window::WindowMode,
-};
+use bevy_inspector_egui::prelude::*;
 
+use bevy::{input::common_conditions::input_just_pressed, prelude::*, window::WindowMode};
+
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use rand::Rng;
 
 use crate::{
@@ -21,36 +21,32 @@ mod trail;
 mod window;
 
 /// Defines a physical object
-#[derive(Component, Clone, Copy, Debug, Default)]
+#[derive(Component, Clone, Copy, Debug, Default, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
 pub struct Object;
 
 /// Position of a body in world space.
-/// Split into previous/current for interpolation.
-#[derive(Component, Clone, Copy, Debug, Default)]
-pub struct Position {
-    pub current: Vec2,
-    pub previous: Vec2,
-}
+#[derive(Component, Clone, Copy, Debug, Default, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
+pub struct Position(Vec2);
 
-impl Position {
-    fn new(position: Vec2) -> Position {
-        Position {
-            current: position,
-            previous: position,
-        }
-    }
-}
+#[derive(Component, Clone, Copy, Debug, Default, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
+pub struct LastPos(Vec2);
 
 /// Linear velocity (m/s)
-#[derive(Component, Clone, Copy, Debug, Default, Deref, DerefMut)]
+#[derive(Component, Clone, Copy, Debug, Default, Deref, DerefMut, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
 pub struct Velocity(pub Vec2);
 
-/// Linear acceleration (m/s²)
-#[derive(Component, Clone, Copy, Debug, Default, Deref, DerefMut)]
-pub struct Acceleration(pub Vec2);
+/// Linear force (N)
+#[derive(Component, Clone, Copy, Debug, Default, Deref, DerefMut, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
+pub struct Force(pub Vec2);
 
 /// Mass (kg). Must be positive and nonzero.
-#[derive(Component, Clone, Copy, Debug)]
+#[derive(Component, Clone, Copy, Debug, Reflect, InspectorOptions)]
+#[reflect(Component, InspectorOptions)]
 pub struct Mass(pub f32);
 
 impl Default for Mass {
@@ -62,8 +58,9 @@ impl Default for Mass {
 #[derive(Bundle, Clone, Debug, Default)]
 pub struct PhysicsBundle {
     pub position: Position,
+    pub last_pos: LastPos,
     pub velocity: Velocity,
-    pub acceleration: Acceleration,
+    pub force: Force,
     pub mass: Mass,
     pub object: Object,
 
@@ -88,6 +85,8 @@ enum SimulationState {
 )]
 pub struct ObjectAssets(pub Handle<Mesh>);
 
+const GRAVITY_CONSTANT: f32 = 6.67e-11;
+
 fn main() {
     let window_plugin = WindowPlugin {
         primary_window: Some(Window {
@@ -101,6 +100,8 @@ fn main() {
     App::new()
         .insert_resource(Time::<Fixed>::from_hz(20.0))
         .add_plugins(DefaultPlugins.set(window_plugin))
+        .add_plugins(EguiPlugin::default())
+        .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(CameraPlugin)
         .add_plugins(WorldCursorPlugin)
         // .add_plugins(TrailPlugin)
@@ -115,9 +116,18 @@ fn main() {
         )
         .add_systems(
             FixedUpdate,
-            physics_step.run_if(in_state(SimulationState::Running)),
+            (
+                interact_bodies.run_if(in_state(SimulationState::Running)),
+                physics_step.run_if(in_state(SimulationState::Running)),
+            )
+                .chain(),
         )
         .insert_state(SimulationState::Running)
+        .register_type::<Position>()
+        .register_type::<LastPos>()
+        .register_type::<Velocity>()
+        .register_type::<Force>()
+        .register_type::<Mass>()
         .run();
 }
 
@@ -148,8 +158,7 @@ fn spawn_object(
         let position = cursor.single().unwrap().0;
         commands.spawn((
             PhysicsBundle {
-                position: Position::new(position),
-                acceleration: Acceleration(Vec2::new(0., 2.)),
+                position: Position(position),
                 transform: Transform::from_translation(position.extend(0.))
                     .with_scale(Vec3::splat((3. * mass / (density * 4. * PI)).cbrt())),
                 ..Default::default()
@@ -178,27 +187,51 @@ fn toggle_pause(
     }
 }
 
+fn interact_bodies(mut query: Query<(&Mass, &Position, &mut Force)>) {
+    let mut iter = query.iter_combinations_mut();
+    while let Some([(Mass(m1), pos1, mut f1), (Mass(m2), pos2, mut f2)]) = iter.fetch_next() {
+        let delta = pos2.0 - pos1.0;
+        let distance_sq: f32 = delta.length_squared();
+
+        // G / r^2
+        let f = GRAVITY_CONSTANT / distance_sq;
+        let force = f * *m1 * *m2 * delta.normalize();
+        f1.0 += force;
+        f2.0 -= force;
+    }
+}
+
 fn physics_step(
-    mut q: Query<(&mut Position, &mut Velocity, &Acceleration)>,
+    mut q: Query<(
+        &mut Position,
+        &mut LastPos,
+        &mut Velocity,
+        &mut Force,
+        &Mass,
+    )>,
     time: Res<Time<Fixed>>,
 ) {
-    let dt = time.delta_secs();
-    for (mut pos, mut vel, acc) in &mut q {
-        vel.0 += acc.0 * dt;
-        pos.previous = pos.current;
-        pos.current += vel.0 * dt;
+    // convert real seconds into simulated years
+    let dt = time.delta_secs() * 3600. * 24. * 356.25;
+
+    for (mut pos, mut last_pos, mut vel, mut f, m) in &mut q {
+        let acc = f.0 / m.0;
+        vel.0 += acc * dt;
+        last_pos.0 = pos.0;
+        pos.0 += vel.0 * dt;
+        f.0 = Vec2::splat(0.);
     }
 }
 
 fn interpolate_visuals(
-    mut q: Query<(&Position, &mut Transform), With<Object>>,
+    mut q: Query<(&Position, &LastPos, &mut Transform), With<Object>>,
     time: Res<Time>,
     fixed_time: Res<Time<Fixed>>,
 ) {
     let fixed_dt = fixed_time.delta_secs();
     let alpha = ((time.elapsed_secs() - fixed_time.elapsed_secs()) / fixed_dt).clamp(0.0, 1.0);
 
-    for (pos, mut transform) in q.iter_mut() {
-        transform.translation = pos.previous.lerp(pos.current, alpha).extend(0.);
+    for (pos, last_pos, mut transform) in q.iter_mut() {
+        transform.translation = last_pos.0.lerp(pos.0, alpha).extend(0.);
     }
 }
